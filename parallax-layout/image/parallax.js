9340c5440d51b6109aeca1800b26f673fac02668
Axsusia <zion437@gmail.com>
2014-07-13 18:21:29 +0900

    my mac

--- parallax-layout/image/parallax.js
+++ parallax-layout/image/parallax.js
@@ -20,8 +20,8 @@
 			$(window).scroll(function() {
 				// Scroll the background at var speed
 				// the yPos is a negative value because we're scrolling it UP!
-				//var yPos = -(($(window).scrollTop() - $bgobj.offset().top) / $bgobj.data('speed'));
-				var yPos = (50- ($(window).scrollTop()/$bgobj.offset().top *50 / $bgobj.data('speed')));
+				var yPos = -(($(window).scrollTop() - $bgobj.offset().top) / $bgobj.data('speed'));
+				//var yPos = (50- ($(window).scrollTop()/$bgobj.offset().top *50 / $bgobj.data('speed')));
 				//console.log(($(window).scrollTop() + ' / ' + $bgobj.offset().top) + ' / ' + $bgobj.data('speed') + ' / ' + yPos);
 				// Put together our final background position
 				var coords = '50% ' + yPos + '%';
